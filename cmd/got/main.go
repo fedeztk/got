ff0d0d5d2311fe18188c44d94625d55cce98fe02
fedeztk <federicoserranexus@gmail.com>
2022-05-01 20:56:38 +0200

    oneshot implementation

--- cmd/got/main.go
+++ cmd/got/main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/fedeztk/got/internal/config"
 	"github.com/fedeztk/got/internal/model"
+	"github.com/fedeztk/got/pkg/translator"
 )
 
 var (
@@ -19,11 +21,39 @@ func main() {
 		false,
 		"show version",
 	)
+	oneShot := flag.Bool(
+		"o",
+		false,
+		"one shot",
+	)
+	from := flag.String(
+		"f",
+		"",
+		"from",
+	)
+	to := flag.String(
+		"t",
+		"",
+		"to",
+	)
 	flag.Parse()
 
 	switch {
 	case *showVersion:
 		fmt.Println(gotVersion)
+
+	case *oneShot:
+		if *from == "" || *to == "" {
+			fmt.Println("from and to are required in one shot mode")
+			os.Exit(1)
+		}
+		response, err := translator.Translate(strings.Join(flag.Args(), " "), *from, *to)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(response.PrettyPrint())
+
 	default:
 		model.Run(config.NewConfig())
 	}
